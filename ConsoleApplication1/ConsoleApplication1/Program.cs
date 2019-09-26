cc1bb072ce870e5ccda084c28aec6373a10c6e51
thainhatquang <thaiquang816@gmail.com>
2019-09-26 17:43:46 +0700

    isOdd

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static bool IsOdd(int n)
+        {
+            if (n % 2!= 0)
+                return true;
+            return false;
+        }
+    }
+}
