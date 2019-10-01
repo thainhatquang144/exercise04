dffba7d7beccc72be839492f37864d6e4eeee15d
quang <thaiquang816@gmail.com>
2019-10-01 16:12:13 +0700

    exercise4

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool IsPrime(int n)
+        {
+            int count = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (n % n == 0)
+                {
+                    count++;
+                } if (count < 2)
+                {
+                    return false;
+                }
+            }
+        return true;
+        }
+
+    }
+}
